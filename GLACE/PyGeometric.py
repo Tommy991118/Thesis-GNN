b0535925dafa103a63bbb58f82254a89dae90e6d
Tommy_Amisulashvili <90957089+Tommy991118@users.noreply.github.com>
2023-03-01 09:40:32 +0100

    Add files via upload

--- GLACE/PyGeometric.py
+++ GLACE/PyGeometric.py
@@ -3,7 +3,9 @@ import numpy as np
 import scipy.sparse as sp
 from sklearn.preprocessing import OrdinalEncoder
 # import torch_geometric
-
+import torch
+import torch_geometric
+from torch_geometric.data import Dataset, Data
 
 # Load CSV file"
 df = pd.read_csv('data/cora_ml/dummydata.csv', delimiter=";")
@@ -30,8 +32,9 @@ encoded_features = encoder.fit_transform(some_features)
 
 # replace the original "some features" column with the encoded values
 df['some features'] = encoded_features
-##############################
-
+###############################################
+ 
+print(df)
 # Create sparse feature matrix. Extracts the values of the features from the dataframe.
 features = sp.csr_matrix(df['some features'].values)
 
@@ -41,14 +44,19 @@ row = node_indices[:-1]
 col = node_indices[1:]
 adj_matrix = sp.csr_matrix((np.ones(len(row)), (row, col)), shape=(num_nodes, num_nodes))
 
+edges = np.array(list(node_id_to_index.items()))
+print(edges)
+
 # Below documentation shows that the attribute features like data/indices/indptr can be extracted from a compressed Sparse row matrix
 
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html
 
+# data = Data(x=features, edge_index=)
+
 # Save as npz file
-np.savez('data/cora_ml/your_graph.npz', adj_data=adj_matrix.data, adj_indices=adj_matrix.indices, adj_indptr=adj_matrix.indptr,
+np.savez('data/cora_ml/your_graph.npz', edges=edges, adj_data=adj_matrix.data, adj_indices=adj_matrix.indices, adj_indptr=adj_matrix.indptr,
          adj_shape=adj_matrix.shape, attr_data=features.data, attr_indices=features.indices, attr_indptr=features.indptr,
-         attr_shape=features.shape, labels=df["labels"])
+         attr_shape=features.shape, labels=np.asarray(df["labels"]))
 
 # file = torch_geometric.parse_npz("your_graph.npz")
 
